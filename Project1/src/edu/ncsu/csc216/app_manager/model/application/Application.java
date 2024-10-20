22b91852629a43933661d11beb94a0a173c886a8
apiduru <apiduru@ncsu.edu>
2024-10-20 15:53:35 -0400

    [Implementation] App type is old after accepting Interview

--- Project1/src/edu/ncsu/csc216/app_manager/model/application/Application.java
+++ Project1/src/edu/ncsu/csc216/app_manager/model/application/Application.java
@@ -571,6 +571,7 @@ public class Application {
 						|| command.getReviewerId().isEmpty()) {
 					throw new UnsupportedOperationException("Invalid information.");
 				}
+				setAppType(A_OLD);
 				setReviewer(command.getReviewerId());
 				setState(REFCHK_NAME);
 				addNote(command.getNote());

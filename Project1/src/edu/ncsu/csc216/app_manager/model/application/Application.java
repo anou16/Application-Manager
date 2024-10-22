53b7fa6c8f8236baa9f1f36c95219e6d564b594f
apiduru <apiduru@ncsu.edu>
2024-10-22 13:31:59 -0400

    [Implementation] Unexpected throw in Offer updateState()

--- Project1/src/edu/ncsu/csc216/app_manager/model/application/Application.java
+++ Project1/src/edu/ncsu/csc216/app_manager/model/application/Application.java
@@ -745,9 +745,9 @@ public class Application {
 		 *                                       current state.
 		 */
 		public void updateState(Command command) {
-			if (command.getNote().isEmpty()) {
-				throw new UnsupportedOperationException("Invalid information.");
-			}
+			// if (command.getNote().isEmpty()) {
+			// throw new UnsupportedOperationException("Invalid information.");
+			// }
 			switch (command.getCommand()) {
 			case ACCEPT:
 				if (reviewer == null || reviewer.isEmpty() || !isProcessed()) {

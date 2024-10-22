dbb37e1921d18de1c13a73447e859890345e728a
apiduru <apiduru@ncsu.edu>
2024-10-22 15:58:40 -0400

    [Implementation] Added a check in constructor

--- Project1/src/edu/ncsu/csc216/app_manager/model/application/Application.java
+++ Project1/src/edu/ncsu/csc216/app_manager/model/application/Application.java
@@ -163,6 +163,9 @@ public class Application {
 		if ((state == INTERVIEW_NAME || state == REFCHK_NAME || state == OFFER_NAME) && (appType == A_NEW)) {
 			throw new IllegalArgumentException("Application cannot be created.");
 		}
+		if ((state == REVIEW_NAME && appType == A_OLD) && (resolution != null)) {
+			throw new IllegalArgumentException("Application cannot be created.");
+		}
 		setAppId(id);
 		setState(state);
 		setAppType(appType);

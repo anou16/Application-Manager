7d6b75fd25f66df3000b95f999b8733b2dc21d62
apiduru <apiduru@ncsu.edu>
2024-10-22 15:43:41 -0400

    [Implementation] Added a check in constructor

--- Project1/src/edu/ncsu/csc216/app_manager/model/application/Application.java
+++ Project1/src/edu/ncsu/csc216/app_manager/model/application/Application.java
@@ -153,7 +153,7 @@ public class Application {
 	 */
 	public Application(int id, String state, String appType, String summary, String reviewer, boolean confirmed,
 			String resolution, ArrayList<String> notes) {
-		if ((state == INTERVIEW_NAME || state == OFFER_NAME || state == REFCHK_NAME)
+		if ((state == INTERVIEW_NAME || state == OFFER_NAME || state == REFCHK_NAME || state == WAITLIST_NAME)
 				&& (reviewer == null || reviewer.isEmpty())) {
 			throw new IllegalArgumentException("Application cannot be created.");
 		}

9268b6291b022d21e9b19ac13044f907abe2d559
apiduru <apiduru@ncsu.edu>
2024-10-21 23:57:16 -0400

    [Implementation] Throw in Interview updateState

--- Project1/src/edu/ncsu/csc216/app_manager/model/application/Application.java
+++ Project1/src/edu/ncsu/csc216/app_manager/model/application/Application.java
@@ -589,9 +589,9 @@ public class Application {
 		 *                                       current state.
 		 */
 		public void updateState(Command command) {
-			// if (command == null || !isProcessed() || command.getNote().isEmpty()) {
-			// throw new UnsupportedOperationException("Invalid information.");
-			// }
+			if (command.getNote().isEmpty() || command.getReviewerId().isEmpty() || !isProcessed()) {
+				throw new UnsupportedOperationException("Invalid information.");
+			}
 			switch (command.getCommand()) {
 			case ACCEPT:
 				setReviewer(command.getReviewerId());
